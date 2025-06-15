d0f72166e4ad4d0fe75d078368f1efba836ba912
Aryan181206 <aryan180906@gmail.com>
2025-06-16 03:26:44 +0530

    working on authentication (not completed)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.google.android.material:material:1.11.0")
+
 }
--- app/src/main/java/com/example/trinetraai/Authentication/SignUp.kt
+++ app/src/main/java/com/example/trinetraai/Authentication/SignUp.kt
@@ -2,10 +2,13 @@ package com.example.trinetraai.Authentication
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.trinetraai.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,34 @@ class SignUp : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val postField = findViewById<TextInputEditText>(R.id.editTextPost)
+        val locationField = findViewById<TextInputEditText>(R.id.editTextLocation)
+
+
+
+        //TODO( complete this)
+        val postOptions = arrayOf("Constable", "Head Constable", "SI", "Inspector", "ACP", "DCP")
+        val zoneOptions = arrayOf("Karol Bagh Zone 56", "Rohini Zone 34", "Dwarka Zone 78", "North Zone 12", "South Zone 90")
+
+
+        postField.setOnClickListener {
+            MaterialAlertDialogBuilder(this, R.style.DarkAlertDialog) // Apply dark style
+                .setTitle("Select Post")
+                .setItems(postOptions) { dialog, which ->
+                    postField.setText(postOptions[which])
+                }
+                .show()
+        }
+
+        locationField.setOnClickListener {
+            val builder = MaterialAlertDialogBuilder(this, R.style.DarkAlertDialog)
+                .setTitle("Select Zone")
+                .setItems(zoneOptions) { dialog, which ->
+                    locationField.setText(zoneOptions[which])
+                }
+            builder.show()
+        }
+
     }
 }

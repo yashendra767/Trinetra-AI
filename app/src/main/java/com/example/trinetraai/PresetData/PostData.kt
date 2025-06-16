25f85378a57825f19376c154b79ec48f4828d97e
Aryan181206 <aryan180906@gmail.com>
2025-06-16 16:53:13 +0530

    Add

---
+++ app/src/main/java/com/example/trinetraai/PresetData/PostData.kt
@@ -0,0 +1,20 @@
+package com.example.trinetraai.PresetData
+
+object PostData {
+    val postList = arrayOf(
+        "Director General of Police (DGP)",
+        "Additional Director General of Police (ADGP)",
+        "Inspector General of Police (IGP)",
+        "Deputy Inspector General of Police (DIG)",
+        "Senior Superintendent of Police (SSP)",
+        "Superintendent of Police (SP)",
+        "Additional Superintendent of Police (Addl. SP)",
+        "Deputy Superintendent of Police (DSP)",
+        "Assistant Superintendent of Police (ASP)",
+        "Inspector of Police",
+        "Sub-Inspector (SI)",
+        "Assistant Sub-Inspector (ASI)",
+        "Constable"
+    )
+
+}

b11bdaa13a17d639d16f862f0ab6cc0443398a9a
yashe <yashendraawasthi@gmail.com>
2025-06-23 14:11:30 +0530

    profile settings open

--- app/src/main/java/com/example/trinetraai/drawer_activities/Settings.kt
+++ app/src/main/java/com/example/trinetraai/drawer_activities/Settings.kt
@@ -12,7 +12,7 @@ class Settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainSettings)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets

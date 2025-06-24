ba01f2f6c4bafed2bbf41e553bbb37dd1c717b88
yashe <yashendraawasthi@gmail.com>
2025-06-24 13:51:28 +0530

    As aryan sir directed

--- app/src/main/java/com/example/trinetraai/Profile.kt
+++ app/src/main/java/com/example/trinetraai/Profile.kt
@@ -11,10 +11,5 @@ class Profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainProfile)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
